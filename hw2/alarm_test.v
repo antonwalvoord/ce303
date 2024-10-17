c03a6a146a73608efddd4017660bacb3063909df
antonwalvoord <147010515+antonwalvoord@users.noreply.github.com>
2024-10-17 14:42:53 -0500

    fix alkl of it

--- hw2/alarm_test.v
+++ hw2/alarm_test.v
@@ -10,15 +10,19 @@ module alarm_test;
 
     initial
         begin
+            // first case - should be alarm = 1
             assign PANIC = 1;
             assign EN = 1;
             assign EXIT = 0;
-            assign WINDOW = 0;
-            assign DOOR = 0;
-            assign GARAGE = 0;
-            #20 assign PANIC = 0;
+            assign WINDOW = 1;
+            assign DOOR = 1;
             #20 assign GARAGE = 1;
+            // second case - should be alarm = 0
+            #20 assign PANIC = 0;
+            // third case - should be alarm = 1
+            #20 assign GARAGE = 1;
+            // fourth case - should be alarm = 0
             #20 assign EN = 0;
-            $finish;
+            #20 $finish;
         end
 endmodule
