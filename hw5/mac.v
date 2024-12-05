14b0b636d5055d14eea097395e2ae239929cd276
antonwalvoord <147010515+antonwalvoord@users.noreply.github.com>
2024-12-05 06:16:09 -0600

    changed assignments

--- hw5/mac.v
+++ hw5/mac.v
@@ -3,7 +3,7 @@
 module mac(
     input signed [3:0] IN,W, //should be signed
     input clk, rstb,
-    output reg signed [11:0] OUT //should be signed
+    output signed [11:0] OUT //should be signed
 );
 
 reg signed [6:0] A; //should be signed
@@ -13,19 +13,19 @@ reg [3:0] counter;
 always @(posedge clk) begin
 
     if(counter == 9) begin
-        counter = 0;
-        OUT = B;
-        B = 0;
-        A = IN*W;
+        assign counter = 0;
+        assign OUT = B;
+        assign B = 0;
+        assign A = IN*W;
     end else if(counter < 9) begin
-        counter = counter + 1;
-        A = IN*W;
-        B = A+B;
+        assign counter = counter + 1;
+        assign A = IN*W;
+        assign B = A+B;
     end else begin
-        counter = 0;
-        OUT = 0;
-        B = 0;
-        A = 0;
+        assign counter = 0;
+        assign OUT = 0;
+        assign B = 0;
+        assign A = 0;
     end
 
 end
@@ -33,10 +33,10 @@ end
 always @(rstb) begin
 
     if(~rstb) begin
-        counter <= 0;
-        OUT <= 0;
-        B <= 0;
-        A <= 0;
+        assign counter <= 0;
+        assign OUT <= 0;
+        assign B <= 0;
+        assign A <= 0;
     end
 end
 
