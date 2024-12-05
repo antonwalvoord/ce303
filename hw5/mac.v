0a12b4577eed117ba9645411cb06e79cae4bf513
antonwalvoord <147010515+antonwalvoord@users.noreply.github.com>
2024-12-05 05:02:53 -0600

    bs error

--- hw5/mac.v
+++ hw5/mac.v
@@ -1,8 +1,7 @@
 `timescale 1ns/1ps
 
 module mac(
-    input reg signed [3:0] IN,
-    input reg signed [3:0] W, //should be signed
+    input signed [3:0] IN,W, //should be signed
     input clk, rstb,
     output reg signed [11:0] OUT //should be signed
 );
