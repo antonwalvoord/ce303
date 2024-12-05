a75d17e33cd6f197cfe2b27e9a653afb40bb2857
Anton Walvoord <xpa7059@hanlon.wot.ece.northwestern.edu>
2024-12-05 07:36:36 -0600

    took alu

--- hw4/Synthesis/xcelium.d/run.lnx8664.18.09.d/files.ts
+++ hw4/Synthesis/xcelium.d/run.lnx8664.18.09.d/files.ts
@@ -1,3 +1,4 @@
-1731580846 /home/xpa7059/ce303/hw4/Synthesis/alu_syn.v
-1731580007 /home/xpa7059/ce303/hw4/alu_tb.v
+1731656721 /home/xpa7059/ce303/hw4/Synthesis/NangateOpenCellLibrary.v
+1733392899 /home/xpa7059/ce303/hw4/Synthesis/alu_syn.v
+1733392899 /home/xpa7059/ce303/hw4/alu_tb.v
 1731580007 /home/xpa7059/ce303/hw4/alu.v
