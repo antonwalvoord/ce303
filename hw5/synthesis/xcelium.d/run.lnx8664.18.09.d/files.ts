46276071d77207e249cad392b264693d81b76131
Anton Walvoord <xpa7059@hanlon.wot.ece.northwestern.edu>
2024-12-05 08:13:18 -0600

    idk

--- hw5/synthesis/xcelium.d/run.lnx8664.18.09.d/files.ts
+++ hw5/synthesis/xcelium.d/run.lnx8664.18.09.d/files.ts
@@ -1,3 +1,3 @@
 1568236065 /vol/ece303/genus_tutorial/NangateOpenCellLibrary.v
-1733406425 /home/xpa7059/ce303/hw5/synthesis/mac_syn.v
+1733407812 /home/xpa7059/ce303/hw5/synthesis/mac_syn.v
 1733397607 /home/xpa7059/ce303/hw5/mac_tb.v
